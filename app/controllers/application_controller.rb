8685f7b123a8bc96c1e147cb62df4db6cc1d464f
Njunu-sk <50931990+Njunu-sk@users.noreply.github.com>
2020-11-24 11:35:31 +0300

    added public activities to track products and sales (#6)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include PublicActivity::StoreController
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = "Acess denied"
     redirect_to root_url
