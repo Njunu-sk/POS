b0c82c559f7a6cb602ed12a9df24372bedc065bd
Njunu-sk <simonnjunu16@gmail.com>
2020-11-24 10:59:51 +0300

    add cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "Acess denied"
+    redirect_to root_url
+  end
 end
---
+++ app/models/ability.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+  if user.present?
+    if user.role.name == 'admin'
+      can :manage, :all
+     else
+      can :create, Product
+      can :create, Sale
+      can :read, :all
+     end
+    else
+      can :read, :all
+    end
+  end
+end
